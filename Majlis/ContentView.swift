5b7d5322c22d2ec4c7eddbde0a5e48eb8b24f9e4
sara <saraalialowayridhi@gmail.com>
2026-02-01 10:02:48 +0300

    Initial Commit

---
+++ Majlis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Majlis
+//
+//  Created by Sarah Alowayridhi on 13/08/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Majlis/MajlisApp.swift
@@ -0,0 +1,17 @@
+//
+//  MajlisApp.swift
+//  Majlis
+//
+//  Created by Sarah Alowayridhi on 13/08/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct MajlisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
